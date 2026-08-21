7f30301338249498754e5aca51526aef5f312f10
nicky-wrc <nick.worachatz@gmail.com>
2026-08-22 01:40:05 +0700

    Refacter Portfolio New

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Store generated output in the production directory already ignored by Git.
-  distDir: "build",
   reactCompiler: true,
 };
 
